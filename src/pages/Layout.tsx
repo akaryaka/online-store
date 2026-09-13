3a666ef63a772ce58e5a0e85e899ae1524fc3e30
andrew <akariaka@yandex.ru>
2026-09-13 16:58:25 +0600

    create layout

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import type { Props } from "./Layout.props"
+
+const Layout = ( {children}: Props) => {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
