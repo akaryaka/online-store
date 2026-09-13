b8ebd678a397d8071f7dbef061da12a6a6c49df4
andrew <akariaka@yandex.ru>
2026-09-13 17:01:40 +0600

    extends layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,25 @@
-import CardList from "../components/cardList/cardList"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
 import HomeSection from "../components/HomeSection"
 import heroIcon from './../assets/hero-icon.png'
-import homeList from './../components/cardList/homeList.json'
+import Layout from "./Layout"
 
 const Home = () => {
   return(
     <>
-      <Header />
-      <main className="h-[100vh] bg-[#FBF8EC] pt-[80px]">
-        <div className="hero h-[200px]">
-          <div className="container w-[1440px] ml-[auto] mr-[auto]">
-            <div className="hero__innner pr-[69px]  flex justify-between items-center">
-              <img src={heroIcon} alt="hero-icon" />
-              <h1 className="text-[48px]">Доставка бесплатно от 1000 ₽</h1>
+      <Layout>
+        <main className="h-[100vh] bg-[#FBF8EC] pt-[80px]">
+          <div className="hero h-[200px]">
+            <div className="container w-[1440px] ml-[auto] mr-[auto]">
+              <div className="hero__innner pr-[69px]  flex justify-between items-center">
+                <img src={heroIcon} alt="hero-icon" />
+                <h1 className="text-[48px]">Доставка бесплатно от 1000 ₽</h1>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="container w-[1440px] ml-[auto] mr-[auto]">
-          <HomeSection />
-          
-        </div>
-      </main>
-      <Footer />
+          <div className="container w-[1440px] ml-[auto] mr-[auto]">
+            <HomeSection />
+          </div>
+        </main>
+      </Layout>
     </>
   )
 }
