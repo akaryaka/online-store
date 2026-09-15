e96103f23f74f19fa9614fcd7313450f1ab26452
andrew <akariaka@yandex.ru>
2026-09-16 01:11:11 +0600

    add link

--- src/components/crumbs/Crumbs.tsx
+++ src/components/crumbs/Crumbs.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+
 const Crumbs = () => {
-  return <>crumbs</>;
+  return (
+    <>
+      <div className="p-[24px_0px]">
+        <Link to="/">Главная</Link>
+        <Link to="/">Контакты</Link>
+      </div>
+    </>
+  );
 };
 
 export default Crumbs;
