b0121769854abfd53f1849f686e1b2ba363e2e2f
andrew <akariaka@yandex.ru>
2026-09-16 22:39:21 +0600

    dev pp begin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/container/Container";
 import bannerIcon from "@images/banner-icon.png";
 import bannerBg from "@images/banner-bg.png";
 import Card from "@/components/card/Card";
+import ArrowIcon from "@/components/icons/ArrowIcon";
 
 const Home = () => {
   return (
@@ -12,33 +13,22 @@ const Home = () => {
         style={{ backgroundImage: `url('${bannerBg}')` }}
       >
         <Container>
-          <div className="hero__innner pr-[69px]  flex justify-between items-center">
+          <div className="hero__innner pr-[67px] pl-[55px] flex justify-between">
             <img src={bannerIcon} alt="hero-icon" />
-            <h1 className="text-[48px]">Доставка бесплатно от 1000 ₽</h1>
+            <h1 className="text-[48px] p-[64px_0px] font-bold text-[#414141]">
+              Доставка бесплатно от 1000 ₽
+            </h1>
           </div>
         </Container>
       </div>
-      <main className="bg-[#FBF8EC] pt-[80px] pb-[80px]">
+      <main className="bg-[#FBF8EC] pt-[84px] pb-[80px]">
         <Container>
           <div className="ml-[auto] mr-[auto]">
-            <header className="flex justify-between items-center mb-[60px]">
-              <h2 className="text-[64px] font-bold">Акции</h2>
-              <Link className="flex gap-[18px]" to="/">
+            <header className="flex justify-between items-center mb-[40px] pr-[7px]">
+              <h2 className="text-[36px] font-bold">Акции</h2>
+              <Link className="flex leading-[150%] gap-[29px] mt-[12px]" to="/">
                 <span>Все акции</span>
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M8.64645 5.64645C8.84171 5.45118 9.15829 5.45118 9.35355 5.64645L15.3536 11.6464C15.5488 11.8417 15.5488 12.1583 15.3536 12.3536L9.35355 18.3536C9.15829 18.5488 8.84171 18.5488 8.64645 18.3536C8.45118 18.1583 8.45118 17.8417 8.64645 17.6464L14.2929 12L8.64645 6.35355C8.45118 6.15829 8.45118 5.84171 8.64645 5.64645Z"
-                    fill="#606060"
-                  />
-                </svg>
+                <ArrowIcon />
               </Link>
             </header>
             <div className="grid gap-[40px] grid-cols-4">
@@ -50,23 +40,10 @@ const Home = () => {
           </div>
           <div className="pt-[80px] ml-[auto] mr-[auto]">
             <header className="flex justify-between items-center mb-[60px]">
-              <h2 className="text-[64px] font-bold">Новинки</h2>
+              <h2 className="text-[36px] font-bold">Новинки</h2>
               <Link className="flex gap-[18px]" to="/">
                 <span>Все новинки</span>
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M8.64645 5.64645C8.84171 5.45118 9.15829 5.45118 9.35355 5.64645L15.3536 11.6464C15.5488 11.8417 15.5488 12.1583 15.3536 12.3536L9.35355 18.3536C9.15829 18.5488 8.84171 18.5488 8.64645 18.3536C8.45118 18.1583 8.45118 17.8417 8.64645 17.6464L14.2929 12L8.64645 6.35355C8.45118 6.15829 8.45118 5.84171 8.64645 5.64645Z"
-                    fill="#606060"
-                  />
-                </svg>
+                <ArrowIcon />
               </Link>
             </header>
             <div className="grid gap-[40px] grid-cols-4">
@@ -78,23 +55,10 @@ const Home = () => {
           </div>
           <div className="pt-[80px] ml-[auto] mr-[auto]">
             <header className="flex justify-between items-center mb-[60px]">
-              <h2 className="text-[64px] font-bold">Покупали раньше</h2>
+              <h2 className="text-[36px] font-bold">Покупали раньше</h2>
               <Link className="flex gap-[18px]" to="/">
                 <span>Все покупки</span>
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M8.64645 5.64645C8.84171 5.45118 9.15829 5.45118 9.35355 5.64645L15.3536 11.6464C15.5488 11.8417 15.5488 12.1583 15.3536 12.3536L9.35355 18.3536C9.15829 18.5488 8.84171 18.5488 8.64645 18.3536C8.45118 18.1583 8.45118 17.8417 8.64645 17.6464L14.2929 12L8.64645 6.35355C8.45118 6.15829 8.45118 5.84171 8.64645 5.64645Z"
-                    fill="#606060"
-                  />
-                </svg>
+                <ArrowIcon />
               </Link>
             </header>
             <div className="grid gap-[40px] grid-cols-4">
@@ -103,6 +67,11 @@ const Home = () => {
               <Card />
               <Card />
             </div>
+          </div>
+          <div>
+            <h2 className="text-[36px] font-bold mb-[40px]">
+              Специальные предложения
+            </h2>
           </div>
         </Container>
       </main>
