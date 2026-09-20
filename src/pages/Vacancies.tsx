48f9a880ad13dc101986402e07e2216ff1fe89b1
andrew <akariaka@yandex.ru>
2026-09-20 18:31:58 +0600

    Implement vacancies page

--- src/pages/Vacancies.tsx
+++ src/pages/Vacancies.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/container/Container";
 import Crumbs from "@/components/crumbs/Crumbs";
 import Title from "@/components/title/Title";
+import PhoneIcon from "@/components/icons/PhoneIcon";
 
 const Vacancies = () => {
   return (
@@ -10,8 +11,225 @@ const Vacancies = () => {
           <div className="mb-[24px]">
             <Crumbs page="Вакансии" />
           </div>
-
-          <Title>Вакансии</Title>
+          <Title className="mb-[60px]">Вакансии</Title>
+          <div className="grid grid-cols-3 gap-[40px]">
+            <div className="p-[32px] rounded-[4px] bg-[#fff] shadow-[1px_2px_2px_rgba(0,0,0,0.1)]">
+              <h2 className="text-[24px] leading-[150%] text-[#414141] mb-[16px] font-bold">
+                Должность
+              </h2>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Требования</h3>
+                <p>
+                  Текст про требования текст про требования текст про требования
+                  текст про требования текст про требования
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Обязанности</h3>
+                <p>
+                  Текст про обязаности текст про обязаности текст про обязаности
+                  текст про обязаности текст про обязаности
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Условия</h3>
+                <p>
+                  Текст про условия текст про условия текст про условия текст
+                  про условия текст про условия текст про условия
+                </p>
+              </div>
+              <h3 className="mb-[8px] text-[#414141]">Звоните</h3>
+              <div className="flex items-center gap-[8px]">
+                <PhoneIcon />
+                <a
+                  className="text-[18px] leading-[150%] text-[#414141] underline"
+                  href="tel:+7 904 271 35 90"
+                >
+                  +7 904 271 35 90
+                </a>
+              </div>
+            </div>
+            <div className="p-[32px] rounded-[4px] bg-[#fff] shadow-[1px_2px_2px_rgba(0,0,0,0.1)]">
+              <h2 className="text-[24px] leading-[150%] text-[#414141] mb-[16px] font-bold">
+                Должность
+              </h2>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Требования</h3>
+                <p>
+                  Текст про требования текст про требования текст про требования
+                  текст про требования текст про требования
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Обязанности</h3>
+                <p>
+                  Текст про обязаности текст про обязаности текст про обязаности
+                  текст про обязаности текст про обязаности
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Условия</h3>
+                <p>
+                  Текст про условия текст про условия текст про условия текст
+                  про условия текст про условия текст про условия
+                </p>
+              </div>
+              <h3 className="mb-[8px] text-[#414141]">Звоните</h3>
+              <div className="flex items-center gap-[8px]">
+                <PhoneIcon />
+                <a
+                  className="text-[18px] leading-[150%] text-[#414141] underline"
+                  href="tel:+7 904 271 35 90"
+                >
+                  +7 904 271 35 90
+                </a>
+              </div>
+            </div>
+            <div className="p-[32px] rounded-[4px] bg-[#fff] shadow-[1px_2px_2px_rgba(0,0,0,0.1)]">
+              <h2 className="text-[24px] leading-[150%] text-[#414141] mb-[16px] font-bold">
+                Должность
+              </h2>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Требования</h3>
+                <p>
+                  Текст про требования текст про требования текст про требования
+                  текст про требования текст про требования
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Обязанности</h3>
+                <p>
+                  Текст про обязаности текст про обязаности текст про обязаности
+                  текст про обязаности текст про обязаности
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Условия</h3>
+                <p>
+                  Текст про условия текст про условия текст про условия текст
+                  про условия текст про условия текст про условия
+                </p>
+              </div>
+              <h3 className="mb-[8px] text-[#414141]">Звоните</h3>
+              <div className="flex items-center gap-[8px]">
+                <PhoneIcon />
+                <a
+                  className="text-[18px] leading-[150%] text-[#414141] underline"
+                  href="tel:+7 904 271 35 90"
+                >
+                  +7 904 271 35 90
+                </a>
+              </div>
+            </div>
+            <div className="p-[32px] rounded-[4px] bg-[#fff] shadow-[1px_2px_2px_rgba(0,0,0,0.1)]">
+              <h2 className="text-[24px] leading-[150%] text-[#414141] mb-[16px] font-bold">
+                Должность
+              </h2>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Требования</h3>
+                <p>
+                  Текст про требования текст про требования текст про требования
+                  текст про требования текст про требования
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Обязанности</h3>
+                <p>
+                  Текст про обязаности текст про обязаности текст про обязаности
+                  текст про обязаности текст про обязаности
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Условия</h3>
+                <p>
+                  Текст про условия текст про условия текст про условия текст
+                  про условия текст про условия текст про условия
+                </p>
+              </div>
+              <h3 className="mb-[8px] text-[#414141]">Звоните</h3>
+              <div className="flex items-center gap-[8px]">
+                <PhoneIcon />
+                <a
+                  className="text-[18px] leading-[150%] text-[#414141] underline"
+                  href="tel:+7 904 271 35 90"
+                >
+                  +7 904 271 35 90
+                </a>
+              </div>
+            </div>
+            <div className="p-[32px] rounded-[4px] bg-[#fff] shadow-[1px_2px_2px_rgba(0,0,0,0.1)]">
+              <h2 className="text-[24px] leading-[150%] text-[#414141] mb-[16px] font-bold">
+                Должность
+              </h2>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Требования</h3>
+                <p>
+                  Текст про требования текст про требования текст про требования
+                  текст про требования текст про требования
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Обязанности</h3>
+                <p>
+                  Текст про обязаности текст про обязаности текст про обязаности
+                  текст про обязаности текст про обязаности
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Условия</h3>
+                <p>
+                  Текст про условия текст про условия текст про условия текст
+                  про условия текст про условия текст про условия
+                </p>
+              </div>
+              <h3 className="mb-[8px] text-[#414141]">Звоните</h3>
+              <div className="flex items-center gap-[8px]">
+                <PhoneIcon />
+                <a
+                  className="text-[18px] leading-[150%] text-[#414141] underline"
+                  href="tel:+7 904 271 35 90"
+                >
+                  +7 904 271 35 90
+                </a>
+              </div>
+            </div>
+            <div className="p-[32px] rounded-[4px] bg-[#fff] shadow-[1px_2px_2px_rgba(0,0,0,0.1)]">
+              <h2 className="text-[24px] leading-[150%] text-[#414141] mb-[16px] font-bold">
+                Должность
+              </h2>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Требования</h3>
+                <p>
+                  Текст про требования текст про требования текст про требования
+                  текст про требования текст про требования
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Обязанности</h3>
+                <p>
+                  Текст про обязаности текст про обязаности текст про обязаности
+                  текст про обязаности текст про обязаности
+                </p>
+              </div>
+              <div className="mb-[16px] text-[#414141]">
+                <h3 className="mb-[8px]">Условия</h3>
+                <p>
+                  Текст про условия текст про условия текст про условия текст
+                  про условия текст про условия текст про условия
+                </p>
+              </div>
+              <h3 className="mb-[8px] text-[#414141]">Звоните</h3>
+              <div className="flex items-center gap-[8px]">
+                <PhoneIcon />
+                <a
+                  className="text-[18px] leading-[150%] text-[#414141] underline"
+                  href="tel:+7 904 271 35 90"
+                >
+                  +7 904 271 35 90
+                </a>
+              </div>
+            </div>
+          </div>
         </Container>
       </div>
     </>
