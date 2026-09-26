d431552d3f6538fe269b0d177900d26155b16f72
andrew <akariaka@yandex.ru>
2026-09-26 18:12:42 +0600

    feat(Phone): update component

--- src/components/phone/Phone.tsx
+++ src/components/phone/Phone.tsx
@@ -1,5 +1,20 @@
-const Phone = () => {
-  return <></>;
+import PhoneIcon from "../icons/PhoneIcon";
+import type { Props } from "./Phone.props";
+
+const Phone = ({ number }: Props) => {
+  return (
+    <>
+      <div className="flex items-center gap-[8px]">
+        <PhoneIcon />
+        <a
+          className="text-[18px] leading-[150%] text-[#414141] underline"
+          href="tel:+7 82140 91330"
+        >
+          {number}
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default Phone;
